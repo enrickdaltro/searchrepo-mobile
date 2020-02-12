5840d28f809c928b3ef6831533e3bc88a32e55d3
Enrick <enrickdaltro@gmail.com>
2020-02-12 12:50:02 -0300

    styling form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container} from './styles';
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar Usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
